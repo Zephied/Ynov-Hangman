2f3232cb3208815a0ca4ff5503bc4cdad88a6b86
ganicolas <nicolas.galmiche@ynov.com>
2023-11-07 15:06:30 +0100

    change text in help func

--- display/help.go
+++ display/help.go
@@ -1,5 +1,5 @@
 package display
 
 func Help() {
-	println("Usage: ./hangman [OPTION]... [FILE]...\nOPTION can be a file path of word or --startWith [FILE] to load a saved game.\nFILE is the path of the file containing the words.\n--ascii [pathfile] can be used to display the hangman in ascii art.\n--help display this help.")
+	println("Usage: ./hangman [OPTION]... [FILE]...\nOPTION can be a file path of word or --startWith [FILE] to load a saved game.\nFILE is the path of the file containing the words.\nyou can add [pathfile] to display the hangman in ascii art.\n--help display this help.")
 }
